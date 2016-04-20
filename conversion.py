b759befd10794b1d5d66379792f55cd2369807e0
JulioNervar <julio.nervar@achievewithoutboders.org>
2016-04-20 10:32:32 +0800

    Conv2

--- conversion.py
+++ conversion.py
@@ -3,12 +3,10 @@ con = raw_input("Enter A if you want to convert fahreheit to celsius \nEnter B t
 if con == "A":
    fahrenheit = float(raw_input("Enter a temperature in celsius : "))
    fahrenheit = 9.0/5.0 * fahrenheit + 32
-   print "Fahrenheit : " + str(fahrenheit)
-   
+   print "Fahrenheit : " + str(fahrenheit)  
 elif con == "B":
    celsius = float(raw_input("Enter a temperature in fahrenheit : "))
    celsius = (celsius - 32) * 5.0/9.0
-   print "Celsius : " + str(celsius)
-   
+   print "Celsius : " + str(celsius) 
 else:
    print "You did not choose A or B"
